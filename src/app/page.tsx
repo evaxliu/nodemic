d73b9540cb1af424c84a96334c1c3f4e0cf08acc
evaxliu <evaxinerliu@gmail.com>
2026-09-14 02:06:02 -0700

    Display nodes and edges. Handle delete node.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Editor from "@/components/editor";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-start justify-start bg-zinc-50 font-sans">
+    <div className="">
       <Editor />
     </div>
   );
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,17 +3,19 @@ import { useState, useCallback } from 'react';
 import {
   ReactFlow,
   addEdge,
-  applyNodeChanges,
-  applyEdgeChanges,
   type Node,
   type Edge,
   type OnConnect,
-  type OnNodesChange,
-  type OnEdgesChange,
   Background,
   BackgroundVariant,
   Controls,
   MiniMap,
+  useNodesState,
+  useEdgesState,
+  getIncomers,
+  getOutgoers,
+  getConnectedEdges,
+  OnNodesDelete,
 } from '@xyflow/react';
 // import type { Node, NodeProps } from '@xyflow/react';
 
@@ -27,43 +29,88 @@ const initialNodes: Node[] = [
 const initialEdges: Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
 
 export default function Editor() {
-  const [nodes, setNodes] = useState<Node[]>(initialNodes);
-  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   function createNewNode() {
     setNodes(() => [...nodes, { id: crypto.randomUUID(), position: { x: 50, y: 50 }, data: { label: 'Node 1' } }])
   }
 
-  const onNodesChange: OnNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes],
+  const onNodesDelete: OnNodesDelete = useCallback(
+    (deleted) => {
+      let remainingNodes = [...nodes];
+      setEdges(
+        deleted.reduce((acc, node) => {
+          const incomers = getIncomers(node, remainingNodes, acc);
+          const outgoers = getOutgoers(node, remainingNodes, acc);
+          const connectedEdges = getConnectedEdges([node], acc);
+ 
+          const remainingEdges = acc.filter((edge) => !connectedEdges.includes(edge));
+ 
+          const createdEdges = incomers.flatMap(({ id: source }) =>
+            outgoers.map(({ id: target }) => ({
+              id: `${source}->${target}`,
+              source,
+              target,
+            })),
+          );
+ 
+          remainingNodes = remainingNodes.filter((rn) => rn.id !== node.id);
+ 
+          return [...remainingEdges, ...createdEdges];
+        }, edges),
+      );
+    },
+    [nodes, edges],
   );
-  const onEdgesChange: OnEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges],
-  );
+
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges],
   );
 
   return (
-    <div style={{ width: '60vw', height: '100vh' }} className='flex'>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-        className="text-black"
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-      </ReactFlow>
-      <div>
-        <button onClick={() => createNewNode()}>Add Compartment</button>
+    <div className='flex'>
+      <div style={{ width: '60vw', height: '100vh' }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onNodesDelete={onNodesDelete}
+          onConnect={onConnect}
+          fitView
+          className="text-black"
+        >
+          <Controls />
+          <MiniMap />
+          <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        </ReactFlow>
+      </div>
+      <div className='flex flex-col grow items-start'>
+        <button 
+          onClick={() => createNewNode()} 
+          className='select-none cursor-pointer border m-5 p-2 rounded-2xl'
+        >
+          Add Compartment
+        </button>
+        <div className='m-5'>
+          {nodes.map((node) => 
+            <div key={node.id} className='border-b p-2'>
+              <p>Id: {node.id}</p>
+              <p>Label: {typeof node.data.label === 'string' ? node.data.label : 'No label'}</p>
+            </div>
+          )}
+        </div>
+        <div className='m-5'>
+          {edges.map((edge) => 
+            <div key={edge.id} className='border-b p-2'>
+              <p>Id: {edge.id}</p>
+              <p>Source: {edge.source}</p>
+              <p>Target: {edge.target}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
