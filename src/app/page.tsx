da02b385b1e3d38e41a1d1972d78037523121e6f
evaxliu <evaxinerliu@gmail.com>
2026-09-14 03:02:38 -0700

    Form for creating compartments with id and compartment type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Editor from "@/components/editor";
+import Editor from "@/components/Editor";
 
 export default function Home() {
   return (
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -23,8 +23,8 @@ import {
 // type NonInfectiousNode = Node<{ number: number }, 'Non-infectious'>;
 
 const initialNodes: Node[] = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'S' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'I' } },
 ];
 const initialEdges: Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
 
@@ -32,8 +32,17 @@ export default function Editor() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  function createNewNode() {
-    setNodes(() => [...nodes, { id: crypto.randomUUID(), position: { x: 50, y: 50 }, data: { label: 'Node 1' } }])
+  function createNewNode(e: React.FormEvent<HTMLFormElement>) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const Id = String(formData.get("Id") ?? "");
+    const Label = formData.get("Label");
+    setNodes((nodes) => [...nodes, { id: Id, position: { x: 50, y: 50 }, data: { label: Id+"_"+Label } }]);
+    e.currentTarget.reset();
   }
 
   const onNodesDelete: OnNodesDelete = useCallback(
@@ -88,12 +97,21 @@ export default function Editor() {
         </ReactFlow>
       </div>
       <div className='flex flex-col grow items-start'>
-        <button 
+        <form onSubmit={createNewNode} className='flex items-center gap-2 m-5'>
+          <input name="Id" className='border p-2 rounded-2xl' required placeholder='Id' />
+          <select name="Label" className='border p-3 rounded-2xl' required>
+            <option value="S">S</option>
+            <option value="I">I</option>
+            <option value="R">R</option>
+          </select>
+          <button type="submit" className='select-none cursor-pointer border p-2 rounded-2xl'>Add Compartment</button>
+        </form>
+        {/* <button 
           onClick={() => createNewNode()} 
           className='select-none cursor-pointer border m-5 p-2 rounded-2xl'
         >
           Add Compartment
-        </button>
+        </button> */}
         <div className='m-5'>
           {nodes.map((node) => 
             <div key={node.id} className='border-b p-2'>
